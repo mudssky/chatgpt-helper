a5b215aef546b5ab5f1cb3c77ef2ce94cb9e5e11
mudssky <mudssky@gmail.com>
2023-02-21 00:19:45 +0800

    ✨ feat: initial projects

---
+++ src/vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-monkey/client" />
+//// <reference types="vite-plugin-monkey/global" />
+
+// declare module '$' {
+//   const GM_download: {
+//     (options: DownloadRequest): {
+//       abort(): boolean
+//     }
+//     (url: string, name?: string | undefined): {
+//       abort(): boolean
+//     }
+//   }
+// }
