0f1ba1432bb48374232e0f8df1111ca7c13e9655
mudssky <mudssky@gmail.com>
2023-02-21 10:26:59 +0800

    ✨ feat: add copy markdown

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import html2canvas from 'html2canvas'
-import { GM_cookie, unsafeWindow, monkeyWindow, GM_addElement, GM_download } from '$';
+import { GM_download, } from '$';
+import TurndownService from 'turndown'
+import { turndownPluginGfm } from './vendor/turndown-plugin-gfm'
 // function downloadFile(dataurl: string, filename: string) {
 //   const a = document.createElement('a')
 //   a.style.display = 'none'
@@ -12,30 +14,120 @@ import { GM_cookie, unsafeWindow, monkeyWindow, GM_addElement, GM_download } fro
 //   a.click()
 //   document.body.removeChild(a)
 // }
+function getTurnDownService() {
+  const hljsREG = /^.*(hljs).*(language-[a-z0-9]+).*$/i;
+
+  const gfm = (turndownPluginGfm).gfm
+  //@ts-ignore
+  const turndownService = new TurndownService({
+    hr: '---'
+  }).use(gfm)
+    .addRule('code', {
+      //@ts-ignore
+      filter: (node) => {
+        if (node.nodeName === 'CODE' && hljsREG.test(node.classList.value)) {
+          return 'code';
+        }
+      },
+      //@ts-ignore
+      replacement: (content, node) => {
+        // @ts-ignore
+        const classStr = node.getAttribute('class');
+        if (hljsREG.test(classStr)) {
+          const lang = classStr.match(/.*language-(\w+)/)[1];
+          if (lang) {
+            return `\`\`\`${lang}\n${content}\n\`\`\``;
+          }
+          return `\`\`\`\n${content}\n\`\`\``;
+        }
+      }
+    })
+    .addRule('ignore', {
+      filter: ['button', 'img'],
+      replacement: () => '',
+    })
+    .addRule('table', {
+      filter: 'table',
+      replacement: function (content, node) {
+        return `\`\`\`${content}\n\`\`\``;
+      },
+    });
 
 
+  return turndownService;
+}
+function getFileName() {
+  return (document.querySelectorAll('.dark .overflow-y-auto a.bg-gray-800')[0] as HTMLElement)?.innerText ?? 'snapshot'
+}
 function handleScreenshot() {
   const contentDom = document.querySelectorAll('[class*=react-scroll-to-bottom--css]>.flex.flex-col')[0] ?? document.body
   html2canvas(contentDom as HTMLElement).then(async (canvas) => {
-
     // console.log({ canvas, url: canvas.toDataURL("image/png"), contentDom })
     canvas.toBlob(function (blob: any) {
-      const picName = `${(document.querySelectorAll('.dark .overflow-y-auto a.bg-gray-800')[0] as HTMLElement)?.innerText ?? 'snapshot'}.png`
+      const picName = getFileName() + '.png'
       GM_download({
         url: URL.createObjectURL(blob),
         name: picName,
         onload() {
           console.log('下载图片,', picName)
         },
+        onerror(e) {
+          console.log('下载失败', e)
+        }
       });
     })
   })
 }
+function downloadBlob(blob: Blob, fileName: string) {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.download = fileName ?? 'file';
+  link.href = url;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link)
+}
+function handleCopyMarkdown() {
+  const content = Array.from(document.querySelectorAll('main .items-center>div')).map(i => {
+    let j: any = i.cloneNode(true);
+    if (/dark\:bg-gray-800/.test((i as any).getAttribute('class'))) {
+      j.innerHTML = `<blockquote>${i.innerHTML}</blockquote>`;
+    }
+    return j.innerHTML;
+  }).join('<hr />');
+  const turndownService = getTurnDownService()
+
+
+  const markdown = turndownService.turndown(content)
+
+  const fileName = getFileName() + '.md'
+
+  // const
+  const blob = new Blob([markdown], { type: 'text/markdown' })
+  console.log({ fileName, markdown, blob })
+  downloadBlob(blob, fileName)
+  // GM_download 不支持markdown后缀的文件
+  // GM_download({
+  //   url: URL.createObjectURL(blob),
+  //   name: fileName,
+  //   onload() {
+  //     console.log('下载markdown,', fileName)
+  //   },
+  //   onerror(e) {
+  //     console.log('下载失败', e)
+  //   }
+  // });
+
+}
 </script>
 
 <template>
-  <div class="fixed right-0 bottom-[200px]">
-    <button @click="handleScreenshot" class="btn flex justify-center gap-2 btn-neutral border-0 md:border">截图</button>
+  <div
+    class="fixed right-0 bottom-[50%] w-[200px] transition-all translate-x-[180px] hover:translate-x-[0px]  h-[200px] bg-gray-300 p-[20px] rounded space-y-[10px]">
+    <button @click="handleScreenshot"
+      class="btn flex justify-center gap-2 btn-neutral border-0 md:border">screenshot</button>
+    <button @click="handleCopyMarkdown" class="btn flex justify-center gap-2 btn-neutral border-0 md:border">copy
+      markdown</button>
   </div>
 </template>
 
